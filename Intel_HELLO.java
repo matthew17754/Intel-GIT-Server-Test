7a8dbd055ea7c17a892c5251608b6c6071b23a6f
matthew17754 <matmoradi17@gmail.com>
2017-07-05 08:09:06 -0700

    Initial Test Commit From Intel JF3 Servers

---
+++ Intel_HELLO.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Intel gives its regaurds."); // Display the string.
+    }
+}
+
+
+//Intel gives its regaurds.
